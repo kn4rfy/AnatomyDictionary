f27188bf84a94d67f65b0cb9ac7288e2e4a9e594
Francis Knarfy Elopre <kn4rfy@gmail.com>
2020-02-07 00:21:15 +0800

    Mock data

---
+++ src/mock/roots.js
@@ -0,0 +1,42 @@
+export const roots = [
+	{
+		id: 1,
+		description: 'Abdomen'
+	},
+	{
+		id: 2,
+		description: 'Ab'
+	},
+	{
+		id: 3,
+		description: 'Abducere '
+	},
+	{
+		id: 4,
+		description: 'Abilis'
+	},
+	{
+		id: 5,
+		description: 'Abilitas'
+	},
+	{
+		id: 6,
+		description: 'Abruptio'
+	},
+	{
+		id: 7,
+		description: 'Absolvere'
+	},
+	{
+		id: 8,
+		description: 'Absorbare'
+	},
+	{
+		id: 9,
+		description: 'Accedere'
+	},
+	{
+		id: 10,
+		description: 'Accomodare'
+	},
+];
---
+++ src/mock/usage.js
@@ -0,0 +1,57 @@
+const usage = [
+	{
+		id: 1,
+		root: 1,
+		description: 'Abdomen' +
+			'Abdomin-' +
+			'Abdominis, “of the abdomen”'
+	},
+	{
+		id: 2,
+		root: 2,
+		description: 'Ab-' +
+			'A-'
+	},
+	{
+		id: 3,
+		root: 3,
+		description: 'Abduct-' +
+			'Abducent'
+	},
+	{
+		id: 4,
+		root: 4,
+		description: 'Abilis' +
+			'-ilis (Eng., -ile)'
+	},
+	{
+		id: 5,
+		root: 5,
+		description: '-ability '
+	},
+	{
+		id: 6,
+		root: 6,
+		description: 'Absolute'
+	},
+	{
+		id: 7,
+		root: 7,
+		description: 'Absorpt-'
+	},
+	{
+		id: 8,
+		root: 8,
+		description: 'Acantho-'
+	},
+	{
+		id: 9,
+		root: 9,
+		description: 'Access-'
+	},
+	{
+		id: 10,
+		root: 10,
+		description: 'Accomodat-'
+	}
+];
